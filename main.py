bc56cac2ad4f3e642874d56efb16fc6a9eb9be92
Guillaume <guillaume.neirinckx@centran.be>
2021-11-28 15:54:35 +0100

    add txt

--- main.py
+++ main.py
@@ -2,12 +2,21 @@ from pandas_ods_reader import read_ods
 from random import shuffle
 import time
 
-ods_path = "letterPairs.ods"
-sheet_index = 1
+SLEEPING_TIME_SEC = 2
+
+ODS_PATH = "letterPairs.ods"
+SHEET_INDEX = 1
+
+
+def print_pairs_ordered():
+    for first_char in range(ord('A'), ord('V') + 1):
+        for second_char in range(ord('A'), ord('V') + 1):
+            if first_char != second_char:
+                print(chr(second_char) + chr(first_char))
+                time.sleep(SLEEPING_TIME_SEC)
 
 
 def test_pairs():
-    print("Hello, pro blind solver\n")
     pair_to_word = build_pair_to_word_map()
     pairs = list(pair_to_word.keys())
     shuffle(pairs)
@@ -16,11 +25,11 @@ def test_pairs():
         print("Answer = " + answer)
         print(pair + ' ?')
         answer = pair_to_word[pair]
-        time.sleep(2)
+        time.sleep(SLEEPING_TIME_SEC)
 
 
 def build_pair_to_word_map():
-    df = read_ods(ods_path, sheet_index)
+    df = read_ods(ODS_PATH, SHEET_INDEX)
     matrix = df.values.tolist()
     first_char = ord('A')
     pair_to_word = {}
@@ -29,7 +38,7 @@ def build_pair_to_word_map():
             second_char = ord('A')
             for word in row:
                 if word is not None and len(word) > 1 and word != "empty":
-                    pair_to_word[chr(first_char)+chr(second_char)] = word
+                    pair_to_word[chr(first_char) + chr(second_char)] = word
                 second_char += 1
             print("")
             first_char += 1
@@ -37,4 +46,5 @@ def build_pair_to_word_map():
 
 
 if __name__ == '__main__':
+    # print_pairs_ordered()
     test_pairs()
